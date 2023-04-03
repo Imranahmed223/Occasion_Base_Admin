0fcecf9bd55cca22ddabe11a98a77a85560809b4
ishmamAli786 <ishmamali2200@gmail.com>
2023-04-03 23:16:04 +0500

    faq page completed

--- src/module/faq/Faq.jsx
+++ src/module/faq/Faq.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./faq.scss";
+import { AiFillDelete } from "react-icons/ai";
 
 const Faq = () => {
   return (
-    <div>Faq</div>
-  )
-}
+    <>
+      <div className="faq_container">
+        <p className="faq_container_heading">Add FAQ</p>
+        <div className="faq_container_input">
+          <label>Question</label>
+          <br />
+          <input type="text" />
+        </div>
+        <div className="faq_container_input">
+          <label>Answer</label>
+          <br />
+          <textarea></textarea>
+        </div>
+        <button className="question_button">Save</button>
+      </div>
+      <table className="faq_table" border="1">
+        <thead>
+          <tr>
+            <th colspan="3">Question</th>
+            <th colspan="7">Answer</th>
+            <th colspan="2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colspan="3">First Question</td>
+            <td colspan="7">First Answer</td>
+            <td colspan="2">
+              <AiFillDelete />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-export default Faq
+export default Faq;
