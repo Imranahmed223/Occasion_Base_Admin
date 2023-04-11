9805d8ea7a6e93e22cdfbeffcec9bd4b87c7b2a1
ishmamAli786 <ishmamali2200@gmail.com>
2023-04-11 23:56:59 +0500

    promo code module completed

--- src/module/promoCode/AddPromoCode.jsx
+++ src/module/promoCode/AddPromoCode.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./PromoCode.scss";
+import { backArrow } from "./../../assests";
 
 const AddPromoCode = () => {
-  return <div>AddPromoCode</div>;
+  return (
+    <>
+      <div className="add_promo_container_heading">
+        <img src={backArrow} alt="backArrow" />
+        <p>Back</p>
+      </div>
+      <div className="add_promo_container_box">
+        <p className="add_promo_container_box_label">Promo Code Name</p>
+        <input type="text" className="add_promo_container_box_input_1" />
+        <p className="add_promo_container_box_label">Discount (%)</p>
+        <span className="add_promo_container_box_span_1"> - </span>
+        <input type="text" className="add_promo_container_box_input_2" />{" "}
+        <span className="add_promo_container_box_span_2"> + </span>
+        <p className="add_promo_container_box_label">Expires on: (dd/mm/yy)</p>
+        <div>
+          <input type="text" className="add_promo_container_box_input_3" />
+          <input type="text" className="add_promo_container_box_input_3" />
+          <input type="text" className="add_promo_container_box_input_3" />
+        </div>
+        <div className="add_promo_container_btn">
+          <button className="add_promo_container_btn_txt">Save</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddPromoCode;
--- src/module/promoCode/ShowPromoCode.jsx
+++ src/module/promoCode/ShowPromoCode.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./PromoCode.scss";
+import { backArrow, deleteIcon } from "./../../assests";
+import { useNavigate } from "react-router-dom";
 
 const ShowPromoCode = () => {
-  return <div>ShowPromoCode</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="show_promo_container">
+        <div className="show_promo_container_heading">
+          <img src={backArrow} alt="backArrow" />
+          <p>Back</p>
+        </div>
+        <div className="show_promo_container_btn">
+          <button
+            className="show_promo_container_btn_txt"
+            onClick={() => navigate("/addPromoCode")}
+          >
+            New Promo Codes
+          </button>
+        </div>
+      </div>
+      <div className="show_promo_list">
+        <div className="show_promo_list_menu">
+          <p className="show_promo_list_main">Promo Code</p>
+          <p className="show_promo_list_sub_menu">Ocassion123</p>
+          <p className="show_promo_list_second_menu show_promo_list_main">
+            Discount
+          </p>
+          <p className="show_promo_list_sub_menu">10%</p>
+        </div>
+        <div>
+          <img
+            src={deleteIcon}
+            alt="deleteIcon"
+            className="show_promo_list_image"
+          />
+        </div>
+      </div>
+
+      <div className="show_promo_list">
+        <div className="show_promo_list_menu">
+          <p className="show_promo_list_main">Promo Code</p>
+          <p className="show_promo_list_sub_menu">Ocassion123</p>
+          <p className="show_promo_list_second_menu show_promo_list_main">
+            Discount
+          </p>
+          <p className="show_promo_list_sub_menu">10%</p>
+        </div>
+        <div>
+          <img
+            src={deleteIcon}
+            alt="deleteIcon"
+            className="show_promo_list_image"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShowPromoCode;
