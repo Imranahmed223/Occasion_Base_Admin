9a4135721afe11f327473edac180c494876dfb56
ishmamAli786 <ishmamali2200@gmail.com>
2023-04-19 18:08:01 +0500

    vendor accept reject form completed

--- src/module/acceptRejectForm/AcceptRejectForm.jsx
+++ src/module/acceptRejectForm/AcceptRejectForm.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AcceptRejectForm.scss";
 import { vendorFormRightImage } from "./../../assests";
 
 const AcceptRejectForm = () => {
+  const [btn1, setBtn1] = useState(false);
+  const [btn2, setBtn2] = useState(false);
+  const [btn3, setBtn3] = useState(true);
+
+  const handleBtn1 = () => {
+    setBtn1(true);
+    setBtn2(false);
+    setBtn3(false);
+  };
+
+  const handleBtn2 = () => {
+    console.log("btn2 is", btn2);
+    console.log("btn1 is", btn1);
+    console.log("btn3 is", btn3);
+    setBtn1(false);
+    setBtn2(true);
+    setBtn3(false);
+  };
+  const handleBtn3 = () => {
+    setBtn1(false);
+    setBtn2(false);
+    setBtn3(true);
+  };
   return (
     <div className="accept_reject_form_container">
       <div className="accept_reject_form_container_left_box">
@@ -24,13 +47,37 @@ const AcceptRejectForm = () => {
       <div className="accept_reject_form_container_right_box">
         <img src={vendorFormRightImage} alt="vendorFormRightImage" />
         <div className="accept_reject_form_container_right_box_button">
-          <button className="accept_reject_form_container_right_box_button_button2">
+          <button
+            className="accept_reject_form_container_right_box_button_btn"
+            style={{
+              backgroundColor: btn1 && "#FFC841",
+              border: btn1 ? "3px solid #FFC841" : "none",
+              cursor: "pointer",
+            }}
+            onClick={() => handleBtn1()}
+          >
             Hold
           </button>
-          <button className="accept_reject_form_container_right_box_button_button3">
+          <button
+            className="accept_reject_form_container_right_box_button_btn"
+            style={{
+              backgroundColor: btn2 && "#FFC841",
+              border: btn2 ? "3px solid #FFC841" : "none",
+              cursor: "pointer",
+            }}
+            onClick={() => handleBtn2()}
+          >
             Reject
           </button>
-          <button className="accept_reject_form_container_right_box_button_button1">
+          <button
+            className="accept_reject_form_container_right_box_button_btn"
+            style={{
+              backgroundColor: btn3 && "#FFC841",
+              border: btn3 ? "3px solid #FFC841" : "none",
+              cursor: "pointer",
+            }}
+            onClick={() => handleBtn3()}
+          >
             Accept
           </button>
         </div>
